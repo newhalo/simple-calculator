813a7ab156a131c6d1cb8ef23c1c2b852debf954
Nguyen Anh Luan <newhalo214@gmail.com>
2021-09-24 10:59:19 +0700

    Update from marter

--- src/modules/Result/Result.js
+++ src/modules/Result/Result.js
@@ -1,73 +1,64 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from 'react'
 
-import "./Result.css";
+import './Result.css'
 
 const fontSizeBase = 83;
 
 const Result = ({ value }) => {
-  const wrapperRef = useRef();
-  const textRef = useRef();
 
-  const handleFormat = (num) => {
-    try {
-      return Number(num).toLocaleString() + (num.endsWith(".") ? "." : "");
-    } catch (error) {
-      return "Error";
+    const wrapperRef = useRef()
+    const textRef = useRef()
+
+    const handleFormat = (num) => {
+        try {
+            return Number(num).toLocaleString() + (num.endsWith('.') ? '.' : '');
+        } catch (error) {
+            return 'Error'
+        }
     }
-  };
 
-  const getFontSize = () => {
-    return parseFloat(
-      window
-        .getComputedStyle(textRef.current, null)
-        .getPropertyValue("font-size")
-    );
-  };
+    const getFontSize = () => {
+        return parseFloat(
+            window
+                .getComputedStyle(textRef.current, null)
+                .getPropertyValue("font-size")
+        );
+    }
 
-  const reduceFontSize = useCallback(
-    (fontSize = fontSizeBase) => {
-      if (
-        fontSize > 0 &&
-        wrapperRef.current &&
-        textRef.current &&
-        wrapperRef.current.clientWidth <=
-          (textRef.current.clientWidth || textRef.current.offsetWidth) + 40
-      ) {
-        fontSize = getFontSize() - 10;
-        textRef.current.style.fontSize = fontSize + "px";
-        return reduceFontSize(fontSize);
-      }
-    },
-    [wrapperRef, textRef]
-  );
+    const reduceFontSize = useCallback((fontSize = fontSizeBase) => {
+        if (fontSize > 0 && wrapperRef.current && textRef.current &&
+            wrapperRef.current.clientWidth <=
+            (textRef.current.clientWidth || textRef.current.offsetWidth) + 40
+        ) {
+            fontSize = (getFontSize() - 10)
+            textRef.current.style.fontSize = fontSize + 'px';
+            return reduceFontSize(fontSize);
+        }
+    }, [wrapperRef, textRef])
 
-  const resetFontSize = () => {
-    textRef.current.style.fontSize = fontSizeBase + "px";
-  };
+    const resetFontSize = () => {
+        textRef.current.style.fontSize = fontSizeBase + 'px';
+    }
 
-  useEffect(() => {
-    resetFontSize();
-    reduceFontSize();
-  }, [value, reduceFontSize]);
+    useEffect(() => {
+        resetFontSize();
+        reduceFontSize();
+    }, [value, reduceFontSize])
 
-  return (
-    <div ref={wrapperRef} id="display" className="outputScreen">
-      <div className="circles">
-        <span className="red"></span>
-        <span className="yellow"></span>
-        <span className="green"></span>
-      </div>
-      <div className="result">
-        <span ref={textRef} style={{ fontSize: fontSizeBase }}>
-          {isNaN(value)
-            ? "Error"
-            : value === "Infinity"
-            ? "Error"
-            : handleFormat(value)}
-        </span>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div ref={wrapperRef} id="display" className="outputScreen">
+            <div className="circles">
+                <span className="red"></span>
+                <span className="yellow"></span>
+                <span className="green"></span>
+            </div>
+            <div className="result">
+                <span ref={textRef} style={{ fontSize: fontSizeBase }}>
+                    {isNaN(value) ? 'Error' : value === 'Infinity' ? 'Error' : handleFormat(value)}
+                </span>
+            </div>
+        </div>
+    )
+}
 
-export default Result;
+export default Result
